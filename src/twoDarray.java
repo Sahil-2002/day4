d11f1962f190e9b79bbc4fe9c7e0da5d38f1f270
Sahil-2002 <sahila.khandekar@gmail.com>
2024-01-25 15:56:40 +0530

    amend

--- src/twoDarray.java
+++ src/twoDarray.java
@@ -2,26 +2,29 @@ import java.util.Scanner;
 
 public class twoDarray {
     public static void main(String[] args) {
-        int row,col;
+        int row, col;
         Scanner sc = new Scanner(System.in);
         System.out.println("enter the size of row ");
         row = sc.nextInt();
         System.out.println("enter the size of column");
-        col= sc.nextInt();
-        int[][] arr = new int [row ][col];
+        col = sc.nextInt();
+        int[][] arr = new int[row][col];
         System.out.println("enter the number in an array ");
-for(int i =0; i<row; i++){
-    for(int j=0; j<col;j++){
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
 
-        arr[i][j]=sc.nextInt();
+                arr[i][j] = sc.nextInt();
+
+            }
+        }
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+
+
+        }
 
     }
-}
-
-
-
-
-    }
-
-
 }
